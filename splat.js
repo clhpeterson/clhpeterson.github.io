5a75ad091a78f501b4d0085e9233cfd74e0ec5fd
Carsten Peterson <carsten.peterson@yale.edu>
2014-10-23 23:33:26 -0400

    created splat.js with only one circle being animated at a time

---
+++ splat.js
@@ -0,0 +1,46 @@
+var canvas, context, width, height, radius, color, x, y;
+var balls = [];
+
+window.onload = function(){
+	canvas = document.getElementById("canvas");
+	width = window.innerWidth;
+	canvas.width = width;
+	height = window.innerHeight;
+	canvas.height = height;
+	canvas.style.backgroundColor = '#FFFFFF';
+	context = canvas.getContext("2d");
+	color = "#FFFFFF";
+	x = 0;
+	y = 0;
+
+	canvas.addEventListener("click", function(event){
+		x = event.pageX;
+		y = event.pageY;
+		radius = 0;
+		color = randomColor();
+	})
+
+	animate();
+}
+
+var randomColor = function() {
+  return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+}
+
+var draw_circle = function(context, x, y, radius, color) {
+  context.fillStyle = color;
+  context.beginPath();
+  context.arc(x, y, radius, 0, Math.PI *2, false);
+  context.fill();
+}
+
+var animate = function(){
+	requestAnimationFrame(animate);
+	if (radius >= Math.max(width, height)){
+		canvas.style.backgroundColor = color
+	}
+	else {
+		draw_circle(context, x, y, radius, color);
+		radius += 1;
+	}
+}
